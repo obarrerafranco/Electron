7d00c3eefa0127f246a38a8ac2162c6b66c6b653
Orangel Barrera <obarrerafranco@gmail.com>
2017-10-10 11:10:15 -0500

    se crea el selector de imagenes, busqueda de imagenes, se agregan filtros

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
 'use strict'
 
-const { app, BrowserWindow } = require('electron')
+// const { app, BrowserWindow } = require('electron')
+import { app, BrowserWindow } from 'electron'
+import devtools from './devtools'
 
-//console.dir(app)
+if (process.env.NODE_ENV === 'development') {
+  devtools()
+}
+
+// console.dir(app)
 
 app.on('before-quit', () => {
-	console.log('Saliendo...')
+  console.log('Saliendo...')
 })
 
 app.on('ready', () => {
-    let win = new BrowserWindow({
-        //configuracion de la nueva ventana
-        width:800,
-        height:600,
-        title: 'Hola mundo!',
-        center:true,
-        maximizable: false,
-        //fullscreen: true,
-        show: false
-    })
+  let win = new BrowserWindow({
+        // configuracion de la nueva ventana
+    width: 800,
+    height: 600,
+    title: 'Hola mundo!',
+    center: true,
+    maximizable: false,
+        // fullscreen: true,
+    show: false
+  })
 
-//cargar una url
-    win.once('ready-to-show', () => {
-        win.show()
-    })
+// cargar una url
+  win.once('ready-to-show', () => {
+    win.show()
+  })
 
-    //saber la posicion de la ventana
-    win.on ('move', () => {
-        const position = win.getPosition()
-        console.log(`la posición es ${position}`)
-    })
+    // saber la posicion de la ventana
+  win.on('move', () => {
+    const position = win.getPosition()
+    //console.log(`la posición es ${position}`)
+  })
 
-    //cerrar la ventana
-    win.on('closed', () => {
-        win = null
-        app.quit()
-    })
-   win.loadURL(`file://${__dirname}/renderer/index.html`)
+    // cerrar la ventana
+  win.on('closed', () => {
+    win = null
+    app.quit()
+  })
+  win.loadURL(`file://${__dirname}/renderer/index.html`)
    // win.loadURL('http://specialnaturalive.com/mpos')
+  // win.toggleDevTools() //permite ver el inspector de chrome
 })
 
-//app.quit()
+// app.quit()
