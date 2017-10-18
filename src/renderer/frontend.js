81f33915936c471fb1629cbee560198461e02382
Orangel Barrera <obarrerafranco@gmail.com>
2017-10-18 10:09:47 -0500

    Se hace refactor del proyecto

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,8 +1,5 @@
-//import os from 'os'
-import url from 'url'
-import path from 'path'
-import { applyFilter } from './filters'  
 import { setIpc, openDirectory } from './ipcRendererEvents' 
+import { addImagesEvents,selectEvent,searchImagesEvent } from './images-ui'
 
 window.addEventListener('load', () => {
   //document.getElementById('mensaje').innerHTML = 'Este es un mensaje de JS'
@@ -17,68 +14,4 @@ window.addEventListener('load', () => {
 function buttonEvent(id, func) {
   const openDirectory = document.getElementById(id)
   openDirectory.addEventListener('click', func)
-}
-
-function addImagesEvents () {
-  const thumbs = document.querySelectorAll('li.list-group-item')
-
-  for (let i = 0, length1 = thumbs.length; i < length1; i++) {
-    thumbs[i].addEventListener('click', function () {
-      changeImage(this)
-    })
-  }
-}
-
-function selectEvent () {
-  const select = document.getElementById('filters')
-  select.addEventListener('change', function() {
-    console.log(this.value)
-    applyFilter(this.value, document.getElementById('image-displayed'))
-  })
-}
-
-function changeImage (node) {
-  if (node){
-  document.querySelector('li.selected').classList.remove('selected')
-  node.classList.add('selected')
-  document.getElementById('image-displayed').src = node.querySelector('img').src
-}else{
-  document.getElementById('image-displayed').src = ''
-}
-}
-
-
- 
-function searchImagesEvent () {
- const searchBox = document.getElementById('search-box')
-
- searchBox.addEventListener('keyup', function() {
-  const regex = new RegExp(this.value.toLowerCase(), 'gi')
-   if (this.value.length > 0){
-     const thumbs = document.querySelectorAll('li.list-group-item img')
-     for (let i=0, length1 = thumbs.length; i < length1; i++) {
-       const fileUrl = url.parse(thumbs[i].src)
-       const fileName = path.basename(fileUrl.pathname)
-       //console.log(fileName)
-       if (fileName.match(regex)){
-        thumbs[i].parentNode.classList.remove('hidden')
-       }else{
-        thumbs[i].parentNode.classList.add('hidden')
-       }
-     }
-
-     selectFirstImage()
-
-   }else{
-     const hidden = document.querySelectorAll('li.hidden')
-     for (let i=0, lengh1 = hidden.length; i < lengh1; i++){
-       hidden[i].classList.remove('hidden')
-     }
-   }
- })
-}
-
-function selectFirstImage () {
-  const image = document.querySelector('li.list-group-item:not(.hidden)')
-  changeImage()
 }
---
+++ src/renderer/images-ui.js
@@ -0,0 +1,103 @@
+//import os from 'os'
+import url from 'url'
+import path from 'path'
+import { applyFilter } from './filters'  
+
+function addImagesEvents () {
+    const thumbs = document.querySelectorAll('li.list-group-item')
+  
+    for (let i = 0, length1 = thumbs.length; i < length1; i++) {
+      thumbs[i].addEventListener('click', function () {
+        changeImage(this)
+      })
+    }
+  }
+
+  function loadImages (images) {
+    
+      for(let i = 0, length1 = images.length; i < length1; i++){
+        const imagesList = document.querySelector('ul.list-group')
+        const node = `<li class="list-group-item">
+        <img class="media-object pull-left" src="${images[i].src}" width="32" height="32">
+        <div class="media-body">
+          <strong>${images[i].filename}</strong> 
+          <p>${images[i].size}</p>
+        </div>
+      </li>`
+      imagesList.insertAdjacentHTML('beforeend', node)
+      }
+    }
+    function clearImages () {
+        const oldImages = document.querySelectorAll('li.list-group-item')
+      
+        for(let i = 0, length1 = oldImages.length; i < length1; i++){
+          oldImages[i].parentNode.removeChild(oldImages[i])
+        }
+      }
+
+function changeImage (node) {
+    if (node){
+        const selected =  document.querySelector('li.selected')
+    if (selected){
+        selected.classList.remove('selected')
+    }
+    node.classList.add('selected')
+    document.getElementById('image-displayed').src = node.querySelector('img').src
+  }else{
+    document.getElementById('image-displayed').src = ''
+  }
+  }  
+
+function selectFirstImage () {
+    const image = document.querySelector('li.list-group-item:not(.hidden)')
+    changeImage(image)
+  }
+
+function selectEvent () {
+    const select = document.getElementById('filters')
+    select.addEventListener('change', function() {
+    console.log(this.value)
+    applyFilter(this.value, document.getElementById('image-displayed'))
+    })
+}
+
+function searchImagesEvent () {
+    const searchBox = document.getElementById('search-box')
+   
+    searchBox.addEventListener('keyup', function() {
+     const regex = new RegExp(this.value.toLowerCase(), 'gi')
+      if (this.value.length > 0){
+        const thumbs = document.querySelectorAll('li.list-group-item img')
+        for (let i=0, length1 = thumbs.length; i < length1; i++) {
+          const fileUrl = url.parse(thumbs[i].src)
+          const fileName = path.basename(fileUrl.pathname)
+          //console.log(fileName)
+          if (fileName.match(regex)){
+           thumbs[i].parentNode.classList.remove('hidden')
+          }else{
+           thumbs[i].parentNode.classList.add('hidden')
+          }
+        }
+   
+        selectFirstImage()
+   
+      }else{
+        const hidden = document.querySelectorAll('li.hidden')
+        for (let i=0, lengh1 = hidden.length; i < lengh1; i++){
+          hidden[i].classList.remove('hidden')
+        }
+      }
+    })
+   }
+
+   module.exports = {
+    addImagesEvents: addImagesEvents,
+    changeImage : changeImage,
+    selectFirstImage : selectFirstImage,
+    selectEvent : selectEvent,
+    searchImagesEvent : searchImagesEvent,
+    loadImages : loadImages,
+    clearImages: clearImages
+
+
+}
